762436e29a80e7575c72f0f87d6f768feddb0b18
NikolaZdravkovic1 <zdravkovic.nikola111@gmail.com>
2025-02-26 14:59:15 +0100

    added services for recipe and shopping-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,14 +8,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  @Output() recipeWasSelected=new EventEmitter<Recipe>();
-  recipes:Recipe[]=[
-    new Recipe('A test Recipe','This is simply a test','https://www.allrecipes.com/thmb/qq9s8jlKplKUDEo3Gtk15EAJpHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ALR-recipe-24700-churros-VAT-hero-03-4x3-a7f6af1860934b0385f84ab9f13f2613.jpg'),
-    new Recipe('Another test Recipe','This is simply a test','https://www.allrecipes.com/thmb/qq9s8jlKplKUDEo3Gtk15EAJpHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ALR-recipe-24700-churros-VAT-hero-03-4x3-a7f6af1860934b0385f84ab9f13f2613.jpg')
-  ];
+  
+  recipes: Recipe[];
 
-  onRecipeSelected(recipe:Recipe){
-    this.recipeWasSelected.emit(recipe);
+  constructor(private recipeService:RecipeService) {
+
   }
+  
+  ngOnInit(){
+    this.recipes=this.recipeService.getRecipes();
+  }
+
+  
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,46 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+
+@Injectable()
+export class RecipeService{
+
+    recipeSelected=new EventEmitter<Recipe>();
+
+    private recipes:Recipe[]=[
+        new Recipe(
+            'Tasty Schnitzle',
+            'A super-tasty Schnitzel - just awesome!',
+            'https://www.allrecipes.com/thmb/qq9s8jlKplKUDEo3Gtk15EAJpHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ALR-recipe-24700-churros-VAT-hero-03-4x3-a7f6af1860934b0385f84ab9f13f2613.jpg', 
+             [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fries', 20)  
+              ]),
+        new Recipe(
+            'Big Fat Burger',
+            'What else you need to say?',
+            'https://www.allrecipes.com/thmb/qq9s8jlKplKUDEo3Gtk15EAJpHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ALR-recipe-24700-churros-VAT-hero-03-4x3-a7f6af1860934b0385f84ab9f13f2613.jpg', 
+             [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 2)
+             ]),
+      ];
+
+      constructor (private slService:ShoppingListService){
+
+      }
+
+      ngOnInit(){
+
+      }
+
+      getRecipes(){
+        return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
+      }
+}
